e3d6ade85ae9ab5740cd31940d5ba992f16ed505
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-04 00:13:03 +0100

    added string condition statements

--- src/string-conditions.js
+++ src/string-conditions.js
@@ -3,10 +3,20 @@ const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
 let answerOne
 
+if (STR_ONE === 'Hello') {
+  answerOne = true
+}
+console.log(answerOne)
+
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
 let answerTwo
+
+if (STR_TWO !== 'Hello') {
+  answerTwo = true
+}
+console.log(answerTwo)
 
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
@@ -15,12 +25,25 @@ const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
 let answerThree
 
+if (STR_THREE.length > STR_FOUR.length) {
+  answerThree = true
+}
+console.log(answerThree)
+
 // 4. Use conditional statements to set answerFour to true
 // if STR_FIVE starts and ends with the same character, regardless of case
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
 let answerFour
+
+if (
+  STR_FIVE.at(0).toLowerCase() ===
+  STR_FIVE.at(STR_FIVE.length - 1).toLowerCase()
+) {
+  answerFour = true
+}
+console.log(answerFour)
 
 // 5. Use conditional statements to set answerFive to true
 // if STR_SIX starts and ends with the same character, regardless of case
@@ -29,11 +52,28 @@ const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
 
 let answerFive
 
+if (
+  STR_SIX.at(0).toLowerCase() === STR_SIX.at(STR_SIX.length - 1).toLowerCase()
+) {
+  answerFive = true
+} else {
+  answerFive = false
+}
+console.log(answerFive)
+
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
 let answerSix
+
+const lengthOfSeven = STR_SEVEN.length
+const middleChar = Math.floor(lengthOfSeven / 2 + 1) - 1 // + 1 first to always find the middle. -1 then to counteract the start at 0
+
+if (lengthOfSeven % 2 !== 0) {
+  answerSix = STR_SEVEN[middleChar]
+}
+console.log(answerSix)
 
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
