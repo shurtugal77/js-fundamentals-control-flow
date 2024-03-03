9556d0563c971781162e6b8a9bf75a88ec99108d
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-03 22:26:25 +0100

    completed numeric conditions

--- src/numeric-conditions.js
+++ src/numeric-conditions.js
@@ -10,15 +10,44 @@ const ARRAY_TWO = ['Hello', 'Conditions', NUM_ONE] // eslint-disable-line no-unu
 // or true if it is empty
 let answerOne
 
+if (ARRAY_ONE.length !== 0) {
+  // Arrary is not empty
+  answerOne = false
+} else if (ARRAY_ONE.length === 0) {
+  // Array is empty
+  answerOne = true
+}
+console.log(answerOne)
+
 // 2. Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
 // or true if it is empty
 let answerTwo
 
+if (ARRAY_TWO.length !== 0) {
+  // Array is not empty
+  answerTwo = false
+} else if (ARRAY_TWO.length === 0) {
+  // Array is empty
+  answerTwo = true
+}
+console.log(answerTwo)
+
 // 3. Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
 let answerThree
 
+if (NUM_ONE > NUM_TWO) {
+  answerThree = true
+}
+console.log(answerThree)
+
 // 4. Use conditional statements to set answerFour to true if NUM_ONE or NUM_TWO are included in ARRAY_TWO
 let answerFour
+const isInArray = ARRAY_TWO.includes(NUM_ONE || NUM_TWO) // Returns true if found in array
+
+if (isInArray === true) {
+  answerFour = true
+}
+console.log(answerFour)
 
 // Don't edit the code below this line
 module.exports = {
