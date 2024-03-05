1240aee196c78ded6ba2d9640c8b744a925deee1
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-05 23:42:38 +0100

    added multiple conditions

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -8,6 +8,12 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // AND is less than or equal to the UPPER variable
 let answerOne
 
+if (NUM >= LOWER && NUM <= UPPER) {
+  answerOne = true
+}
+
+console.log(answerOne)
+
 // Task 2
 const STR = null
 
@@ -17,6 +23,14 @@ const STR = null
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
 let answerTwo
+
+if (STR === 'Hello' || STR === 'Goodbye') {
+  answerTwo = true
+} else {
+  answerTwo = false
+}
+
+console.log(answerTwo)
 
 // Task 3
 const AGE = 0
@@ -34,6 +48,21 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+
+if (AGE === 0) {
+  answerThree = 'Baby'
+} else if (AGE >= 1 && AGE <= 4) {
+  answerThree = 'Toddler'
+} else if (AGE >= 5 && AGE <= 12) {
+  answerThree = 'Child'
+} else if (AGE >= 13 && AGE <= 19) {
+  answerThree = 'Teenager'
+} else if (AGE >= 20) {
+  answerThree = 'Adult'
+} else {
+  answerThree = 'Wrong Input'
+}
+console.log(answerThree)
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
